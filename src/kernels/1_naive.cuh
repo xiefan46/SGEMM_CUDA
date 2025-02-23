1e7976b8b052a33315c042682002466676674af2
Fan Xie <xiefan.fxie@bytedance.com>
2025-02-23 17:54:54 +0800

    modify

--- src/kernels/1_naive.cuh
+++ src/kernels/1_naive.cuh
@@ -16,9 +16,11 @@ __global__ void sgemm_naive(int M, int N, int K, float alpha, const float *A,
                             const float *B, float beta, float *C) {
     const int x = blockIdx.x * blockDim.x + threadIdx.x;
     const int y = blockIdx.y * blockDim.y + threadIdx.y;
-    float* sum = 0;
-    for (int k = 0; k < K; k++) {
-      sum += A[y * K + k] * B[k * N + x];
+    if (x < N && y < M) {
+        float sum = 0;
+        for (int k = 0; k < K; k++) {
+            sum += A[y * K + k] * B[k * N + x];
+        }
+        C[y * N + x] = sum * alpha + beta * C[y * N + x];
     }
-    C[y * N + x] = sum * alpha + beta * C[y * N + x];
 }
