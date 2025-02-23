a6454950712e3e028f2d2f21215371886b074802
Fan Xie <xiefan.fxie@bytedance.com>
2025-02-23 17:51:37 +0800

    modify

--- src/kernels/1_naive.cuh
+++ src/kernels/1_naive.cuh
@@ -14,16 +14,11 @@ MxK * KxN = MxN
 
 __global__ void sgemm_naive(int M, int N, int K, float alpha, const float *A,
                             const float *B, float beta, float *C) {
-  const uint x = blockIdx.x * blockDim.x + threadIdx.x;
-  const uint y = blockIdx.y * blockDim.y + threadIdx.y;
-
-  // if statement is necessary to make things work under tile quantization
-  if (x < M && y < N) {
-    float tmp = 0.0;
-    for (int i = 0; i < K; ++i) {
-      tmp += A[x * K + i] * B[i * N + y];
+    const int x = blockIdx.x * blockDim.x + threadIdx.x;
+    const int y = blockIdx.y * blockDim.y + threadIdx.y;
+    float* sum = 0.0;
+    for (int k = 0; k < K; k++) {
+      sum += A[y * K + k] * B[k * N + x];
     }
-    // C = α*(A@B)+β*C
-    C[x * N + y] = alpha * tmp + beta * C[x * N + y];
-  }
+    C[y * n + x] = sum * alpha + beta * C[y * n + x];
 }
