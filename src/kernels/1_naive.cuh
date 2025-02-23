7ecff805820f6e814247f910905327de6394372e
Fan Xie <xiefan.fxie@bytedance.com>
2025-02-23 17:59:33 +0800

    modify

--- src/kernels/1_naive.cuh
+++ src/kernels/1_naive.cuh
@@ -12,31 +12,31 @@ MxK * KxN = MxN
 
 */
 
-__global__ void sgemm_naive(int M, int N, int K, float alpha, const float *A,
-                            const float *B, float beta, float *C) {
-    const int x = blockIdx.x * blockDim.x + threadIdx.x;
-    const int y = blockIdx.y * blockDim.y + threadIdx.y;
-    if (x < N && y < M) {
-        float sum = 0.0;
-        for (int k = 0; k < K; k++) {
-            sum += A[y * K + k] * B[k * N + x];
-        }
-        C[y * N + x] = sum * alpha + beta * C[y * N + x];
-    }
-}
-
 //__global__ void sgemm_naive(int M, int N, int K, float alpha, const float *A,
 //                            const float *B, float beta, float *C) {
-//    const uint x = blockIdx.x * blockDim.x + threadIdx.x;
-//    const uint y = blockIdx.y * blockDim.y + threadIdx.y;
-//
-//    // if statement is necessary to make things work under tile quantization
-//    if (x < M && y < N) {
-//        float tmp = 0.0;
-//        for (int i = 0; i < K; ++i) {
-//            tmp += A[x * K + i] * B[i * N + y];
+//    const int x = blockIdx.x * blockDim.x + threadIdx.x;
+//    const int y = blockIdx.y * blockDim.y + threadIdx.y;
+//    if (x < N && y < M) {
+//        float sum = 0.0;
+//        for (int k = 0; k < K; k++) {
+//            sum += A[y * K + k] * B[k * N + x];
 //        }
-//        // C = α*(A@B)+β*C
-//        C[x * N + y] = alpha * tmp + beta * C[x * N + y];
+//        C[y * N + x] = sum * alpha + beta * C[y * N + x];
 //    }
 //}
+
+__global__ void sgemm_naive(int M, int N, int K, float alpha, const float *A,
+                            const float *B, float beta, float *C) {
+    const uint x = blockIdx.x * blockDim.x + threadIdx.x;
+    const uint y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    // if statement is necessary to make things work under tile quantization
+    if (x < M && y < N) {
+        float tmp = 0.0;
+        for (int i = 0; i < K; ++i) {
+            tmp += A[x * K + i] * B[i * N + y];
+        }
+        // C = α*(A@B)+β*C
+        C[x * N + y] = alpha * tmp + beta * C[x * N + y];
+    }
+}
