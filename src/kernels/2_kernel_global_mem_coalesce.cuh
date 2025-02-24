ab2289b15b91945e25a09ae054c0402aaec4b9a2
Fan Xie <xiefan.fxie@bytedance.com>
2025-02-24 16:11:08 +0800

    modify

--- src/kernels/2_kernel_global_mem_coalesce.cuh
+++ src/kernels/2_kernel_global_mem_coalesce.cuh
@@ -7,18 +7,6 @@
 #include <cuda_runtime.h>
 
 template <const uint BLOCKSIZE>
-__global__ void sgemm_global_mem_coalesce(int M, int N, int K, float alpha,
-                                          const float *A, const float *B,
-                                          float beta, float *C) {
-  const int cRow = blockIdx.x * BLOCKSIZE + (threadIdx.x / BLOCKSIZE);
-  const int cCol = blockIdx.y * BLOCKSIZE + (threadIdx.x % BLOCKSIZE);
+__global__ void sgemm_global_mem_coalesce(int M, int N, int K, float alpha, float* A, float* B, float beta, float* C) {
 
-  // if statement is necessary to make things work under tile quantization
-  if (cRow < M && cCol < N) {
-    float tmp = 0.0;
-    for (int i = 0; i < K; ++i) {
-      tmp += A[cRow * K + i] * B[i * N + cCol];
-    }
-    C[cRow * N + cCol] = alpha * tmp + beta * C[cRow * N + cCol];
-  }
 }
