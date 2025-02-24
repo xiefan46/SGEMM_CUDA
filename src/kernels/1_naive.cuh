a0f46cb093cce312947acc261ec3e2bd578029e7
Fan Xie <xiefan.fxie@bytedance.com>
2025-02-24 16:48:44 +0800

    modify

--- src/kernels/1_naive.cuh
+++ src/kernels/1_naive.cuh
@@ -12,31 +12,31 @@ MxK * KxN = MxN
 
 */
 
-__global__ void sgemm_naive(int M, int N, int K, float alpha, const float *A,
-                            const float *B, float beta, float *C) {
-    const int x = blockIdx.x * blockDim.x + threadIdx.x;
-    const int y = blockIdx.y * blockDim.y + threadIdx.y;
-    if (x < M && y < N) {
-        float sum = 0.0;
-        for (int k = 0; k < K; k++) {
-            sum += A[x * K + k] * B[k * N + y];
-        }
-        C[x * N + y] = sum * alpha + beta * C[x * N + y];
-    }
-}
-
-
-// TODO: 探索这种写法是否是对的，以及为什么比上面的快很多
 //__global__ void sgemm_naive(int M, int N, int K, float alpha, const float *A,
 //                            const float *B, float beta, float *C) {
 //    const int x = blockIdx.x * blockDim.x + threadIdx.x;
 //    const int y = blockIdx.y * blockDim.y + threadIdx.y;
-//    if (x < N && y < M) {
+//    if (x < M && y < N) {
 //        float sum = 0.0;
 //        for (int k = 0; k < K; k++) {
-//            sum += A[y * K + k] * B[k * N + x];
+//            sum += A[x * K + k] * B[k * N + y];
 //        }
-//        C[y * N + x] = sum * alpha + beta * C[y * N + x];
+//        C[x * N + y] = sum * alpha + beta * C[x * N + y];
 //    }
 //}
+
+
+// TODO: 探索这种写法是否是对的，以及为什么比上面的快很多
+__global__ void sgemm_naive(int M, int N, int K, float alpha, const float *A,
+                            const float *B, float beta, float *C) {
+    const int x = blockIdx.x * blockDim.x + threadIdx.x;
+    const int y = blockIdx.y * blockDim.y + threadIdx.y;
+    if (x < N && y < M) {
+        float sum = 0.0;
+        for (int k = 0; k < K; k++) {
+            sum += A[y * K + k] * B[k * N + x];
+        }
+        C[y * N + x] = sum * alpha + beta * C[y * N + x];
+    }
+}
 
