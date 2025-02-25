fc8a375cb29f7441d0139110ec75cf54e1a73a7a
Fan Xie <xiefan.fxie@bytedance.com>
2025-02-25 18:01:28 +0800

    modify

--- src/runner.cu
+++ src/runner.cu
@@ -166,14 +166,14 @@ void run_sgemm_coalesce(int M, int N, int K, float alpha, float *A, float *B,
 
 void run_sgemm_shared_mem_block(int M, int N, int K, float alpha, float *A,
                                 float *B, float beta, float *C) {
-  dim3 gridDim(CEIL_DIV(M, 32), CEIL_DIV(N, 32));
+  dim3 gridDim(CEIL_DIV(N, 32), CEIL_DIV(M, 32));
   dim3 blockDim(32 * 32);
   // L1 cache becomes useless, since we access GMEM only via SMEM, so we carve
   // out all of L1 to SMEM. This doesn't currently make a difference, since
   // occupancy is limited by reg and thread count, but it's good to do anyway.
-  cudaFuncSetAttribute(sgemm_shared_mem_block<32>,
-                       cudaFuncAttributePreferredSharedMemoryCarveout,
-                       cudaSharedmemCarveoutMaxShared);
+//  cudaFuncSetAttribute(sgemm_shared_mem_block<32>,
+//                       cudaFuncAttributePreferredSharedMemoryCarveout,
+//                       cudaSharedmemCarveoutMaxShared);
   sgemm_shared_mem_block<32>
       <<<gridDim, blockDim>>>(M, N, K, alpha, A, B, beta, C);
 }
